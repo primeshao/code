ee87086652ddd4bf39e76d5199988533b0e3f958
primeshao <primeshao@outlook.com>
2019-09-07 18:10:49 +0800

    del

--- C/C Experiment/Experiment 1/1SYB1（TC）.C
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main ()
-{
- printf("programming is fun.\n");
- return 0;
- }
--- C/C Experiment/Experiment 1/1SYB4（TC）.C
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
- printf("He said:\"You are welcome!\"");
- printf("\npath=c:\\news\\tc\n");
- return 0;
-}
